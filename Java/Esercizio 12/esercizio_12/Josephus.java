6387daa0466dda8c4cdbbdf7197b2ac9b9ccae9d
Filip <filipmilosevic333@gmail.com>
2018-06-12 19:32:02 +0200

    Aggiustato Josephus

--- Java/Esercizio 12/esercizio_12/Josephus.java
+++ Java/Esercizio 12/esercizio_12/Josephus.java
@@ -1,42 +1,43 @@
 package esercizio_12;/*
- * Modulo esercizio_12.Josephus:
+/*
+ * Modulo Josephus:
  *
  * Programma per risolvere il problema di Giuseppe Flavio
  * (metodi statici))
  *
  * Ultimo aggiornamento: 10/04/2018
  */
-
-
 public class Josephus {
 
 
   public static int josephus( int n ) {
-  
+
     return josephusRec( new RoundTable(n) );
   }
-  
+
   private static int josephusRec( RoundTable rt ) {
-    
+
     if ( rt.numberOfKnightsIn() > 1 ) {
-      
-      return josephusRec( rt.afterNextKnightQuits() );
+
+        return josephusRec( rt.afterNextKnightQuits() );
     } else {
-    
-      return rt.knightWithJugIn();
+
+        return rt.knightWithJugIn();
     }
   }
 
-  
-  public static void main( String args[] ) {
-  
-    int n = Integer.parseInt( args[0] );
-    
+
+    public static void main( String args[] ) {
+
+        int n = 10;
+        //Soluzione 10
+        System.out.println("La soluzione se abbiamo " + n + " cavalieri è " + josephus(n));
+        //Soluzione per da 1 a 10
+        System.out.println("Soluzioni da 1 cavaliere fino a " + n + " cavalieri");
     for ( int k=1; k<=n; k=k+1 ) {
-      System.out.println( k+"    " + josephus(k) );
+        System.out.println(k + "    " + josephus(k));
     }
   }
 
 
-}  // class esercizio_12.Josephus
-
+}  // class Josephus
